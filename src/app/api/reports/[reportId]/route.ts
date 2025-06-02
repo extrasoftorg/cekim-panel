6fd6a621c74cc0014ae183f1c24954b6362d6d8d
YhkooS <yusufturgut3200@gmail.com>
2025-06-02 23:12:44 +0300

    fix

--- src/app/api/reports/[reportId]/route.ts
+++ src/app/api/reports/[reportId]/route.ts
@@ -2,7 +2,7 @@
 
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request, { params }: { params: { reportId: string } }) {
+export async function GET(_request: Request, { params }: { params: { reportId: string } }) {
     const { reportId } = params;
 
     try {
