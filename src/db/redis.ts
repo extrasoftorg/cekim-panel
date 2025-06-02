567069decaeba1f988050ae94cd785135f15f2cf
YhkooS <yusufturgut3200@gmail.com>
2025-06-02 17:53:13 +0300

    procfile

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,16 +1,23 @@
 import Redis from 'ioredis';
 
-export const redis = new Redis({
-  port: 6379,
-  host: 'localhost',
+const redisUrl = process.env.REDIS_URL;
+
+if (!redisUrl) {
+  throw new Error('REDIS_URL tanımlı değil');
+}
+
+const redis = new Redis(redisUrl, {
+  tls: process.env.REDIS_URL?.startsWith('rediss://')
+    ? { rejectUnauthorized: false }
+    : undefined,
 });
 
 redis.on('connect', () => {
-    console.log('Redis’e bağlanıldı');
+  console.log('Redis’e bağlanıldı');
 });
 
 redis.on('error', (err) => {
-    console.error('Redis bağlantı hatası:', err);
+  console.error('Redis bağlantı hatası:', err);
 });
-  
+
 export default redis;
