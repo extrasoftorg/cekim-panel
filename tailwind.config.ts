fb949a30493a0b10d612a460208d5f17694687e7
YhkooS <yusufturgut3200@gmail.com>
2025-06-03 21:04:08 +0300

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,19 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   darkMode: 'class',
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',  // src klasörü (tüm dosyalar)
-    './pages/**/*.{js,ts,jsx,tsx}',  // sayfa dosyaları
-    './components/**/*.{js,ts,jsx,tsx}',  // bileşenler
+    './src/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
     './layouts/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',  // app router dosyaları
-    './node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}',  // şablonlar
+    './app/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        base: '16px', // Varsayılan font-size’ı sabitle
+      },
+    },
   },
   plugins: [],
 };
